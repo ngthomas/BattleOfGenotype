1d8ee0f5b3e7775b85422231d368ab9be33e619b
ngthomas <tngthomasng@gmail.com>
2015-08-05 17:06:24 -0700

    add more module for coverage bias

--- main/PopMs.py
+++ main/PopMs.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+
+# version required Python3
+# numpy should be > v. 1.8
+# python ~/src/BattleOfGenotype/main/PopMs.py -i ~/data/ref/satro_orig_amplicons.fa -ms ~/src/ms/msdir/ms -qp ~/data/ddrad/lines.fq
+
 '''
     This program takes a reference DNA sequence (from a FASTA file) and
     returns modified DNA reads containing SNPs for a defined number of individuals,
@@ -16,12 +21,35 @@ import os, sys, re, argparse, subprocess
 import numpy as np
 from scipy.stats import nbinom, beta
 import random
+import multiprocessing as mp
+
+# Nucleotide Dictionary :suffix 0 transition 1 - tranversion
+
+nuclDict = {
+'R' : ['G', 'A'],
+'Y' : ['T',  'C'],
+'K' : ['G', 'T'],
+'M' : ['A',  'C'],
+'S' : ['G',  'C'],
+'W' : ['A' , 'T'],
+'A0' : ['G'], 'G0' : ['A'],
+'C0' : ['T'], 'T0' : ['C'],
+'A1' : ['C', 'T'],
+'C1' : ['A', 'G'],
+'T1' : ['A', 'G'],
+'G1' : ['C', 'T'],
+'B': ['C', 'G', 'T'],
+'D': ['A', 'G', 'T'],
+'H': ['A', 'C', 'T'],
+'V': ['C', 'G', 'A'],
+'N' : ['A' ,'T', 'C', 'G']}
+
 
 ''' 
     This function counts the number of fasta entries given the Fasta file path
 '''
 
-def getNumEntries(inFASTA):
+def GetNumEntries(inFASTA):
 
     cmd = " ".join(["grep '>' ",
                     inFASTA,
@@ -32,13 +60,13 @@ def getNumEntries(inFASTA):
 
 def GetPhred(fastQCpath):
    
-    fastQC = open(fastQCpath, 'w')
+    FASTQC = open(fastQCpath, 'r')
     
     #get the maximum length of the read sequence
     
     i=1
     readLen = 0;
-    for line in fastQC:
+    for line in FASTQC:
         if (i%4==0):
             a = len(line.strip())
             readLen = max(a, readLen)
@@ -49,12 +77,14 @@ def GetPhred(fastQCpath):
     qualMatrix = np.empty([45,readLen])
     qualMatrix[:] = 0
 
-    for line in fastQC:
+    for line in FASTQC:
         if (i%4==0):
             for pos, ascii in enumerate(list(line.strip())):
-                indx = encode(ascii)-33
+                indx = min(ord(ascii)-33,44)
                 qualMatrix[indx,pos] += 1
         i = i + 1;
+
+    return qualMatrix
 
 
 # This function writes the beginning header for the VCF file
@@ -86,7 +116,7 @@ def WriteVcfHeader(VcfFile, opts):
     '''
 def readFasta(FastaFile, HeaderOut):
     dnaSeq = ""
-    i = 1
+    i = 0
     for line in FastaFile:
         if re.match(">", line):
             HeaderOut.write("\t".join([str(i), line]))
@@ -110,13 +140,16 @@ def readFasta(FastaFile, HeaderOut):
 '''
 
 
-def SNPit(id, numEntries, seq, SNPsFile, VcfFile, opts):
+def SNPit(id, coverageMatrix, seq, SNPsFile, VcfFile, opts):
     
     nindiv = opts.nindiv
     snpRate = opts.snprate
     #recomb = opts.recomb
     tstvRate = opts.tstv
-    
+    indelRate = opts.ir
+    extendRate = opts.er    
+
+
     snpPos = []
     
     # randomly drawn the number of snps
@@ -131,9 +164,10 @@ def SNPit(id, numEntries, seq, SNPsFile, VcfFile, opts):
         return 0
 
     nucList = list(seq)
-    modelSeq = DivideRef(seq,tstvRate)
+    modelSeq = DivideRef(seq, tstvRate, indelRate, extendRate)
     numSNP = len(snpPos)
 
+    ## passing ms terminal command line via python subprocess
     output = subprocess.check_output(" ".join([opts.ms,
                                                str(nindiv*2),
                                                "1",
@@ -155,7 +189,7 @@ def SNPit(id, numEntries, seq, SNPsFile, VcfFile, opts):
 
     # Parsing out MS output
     for line in output:
-        # this condition searches out for lines (from ms's output) containing information about the variant positions
+        # I am looking out for line that contain any information about the variant positions
         if re.match("positions:", line):
             posLine = line.strip().split(" ")
             haplo_ready = True
@@ -165,16 +199,21 @@ def SNPit(id, numEntries, seq, SNPsFile, VcfFile, opts):
                 if i>1 and snpPos[i-1] == snpPos[i-2]:
                     snpPos[i-1] = snpPos[i-1] +1
     
-        # this condition looks for lines with individual's specificed haplotype configuration e.g. 1001101
+        # this section parses ms' output line on displaying individual's specificed haplotype configuration e.g. 1001101
         elif haplo_ready:
             haplotype = [int(l) for l in list(line)]
             #print(int(indx/2), haplotype, indx, nindiv*2)
+            
+            # storing each haplotype into a matrix
             haplMatrix[int(indx/2),indx%2,] = haplotype
-            seq = RetrieveSeq(haplotype,snpPos,modelSeq)
-            PrintIndivFasta(seq, SNPsFile[int(indx/2)], indx, id)
+
+            indivSeq = RetrieveSeq(haplotype,snpPos,modelSeq)
+            PrintIndivFasta(id, indivSeq, SNPsFile[int(indx/2)], coverageMatrix[int(indx/2),indx%2, id-1])
             indx = indx + 1
+
+            ## stop the process when the number of individuals needed to print out is fulfilled
             if indx == nindiv*2:
-                haplo_ready = False
+                break
 
     PrintRefFasta(modelSeq, SNPsFile[nindiv],id)
     PrintVcf(VcfFile, haplMatrix, snpPos, modelSeq, id)
@@ -197,7 +236,7 @@ def RetrieveSeq(hap,snpPos,modelSeq):
     
     return "".join(modelSeq[indic,range(lenSeq)])
 
-def PrintIndivFasta(seq, SNPsFile, indx, id):
+def PrintIndivFasta(id, seq, SNPsFile, numRepeat):
 
     template=""">contig_{id}
 {seq}\n"""
@@ -208,7 +247,7 @@ def PrintIndivFasta(seq, SNPsFile, indx, id):
     "id": id
     }
 
-    SNPsFile.write(template.format(**context))
+    SNPsFile.write(template.format(**context)*numRepeat)
 
 def PrintRefFasta(modelSeq, SNPsFile, id):
     
@@ -246,22 +285,6 @@ def PrintVcf(VcfFile, haplMatrix, snpPos, modelSeq, id):
         VcfFile.write(vcfLine)
 
 
-# Nucleotide Dictionary :suffix 0 transition 1 - tranversion
-
-nuclDict = {
-'R' : ['G', 'A'],
-'Y' : ['T',  'C'],
-'K' : ['G', 'T'],
-'M' : ['A',  'C'],
-'S' : ['G',  'C'],
-'W' : ['A' , 'T'],
-'A0' : ['G'], 'G0' : ['A'],
-'C0' : ['T'], 'T0' : ['C'],
-'A1' : ['C', 'T'],
-'C1' : ['A', 'G'],
-'T1' : ['A', 'G'],
-'G1' : ['C', 'T'],
-'N' : ['A' ,'T', 'C', 'G']}
 
 '''
     This function takes the sequence and transition:transversion
@@ -271,29 +294,43 @@ nuclDict = {
     Current restrictions:
     Transiton vs Transversion: assume equal rate between transition or tranversion pairs
     limited to biallelic variants
-    nucleotide alphabeta: only accepts: A T C G R Y K M S W N
+    nucleotide alphabeta: only accepts: A T C G R Y K M S W N B D H V
     '''
 
-def DivideRef(seq, tstvRate):
+def DivideRef(seq, tstvRate, indelRate, extendRate):
     
     #!!!! w/o the list(); pointer of seq is passed
     majorSeq = list(seq)
     minorSeq = list(seq)
+
     # 1 as Transversion
     tS = np.random.binomial(n=1, p=1.0/(1+tstvRate), size=len(seq))
-    
+
+    # Shall I include insertion/deletion in place of substitution? 1 - yes
+    indelI = np.random.binomial(n=1, p=indelRate, size=len(seq))
+    # If it's an indel, is it an extension? 1 - yes
+    isInsert = np.random.binomial(n=1, p=0.5, size=len(seq))
+
+    # How long will the base extended for an insertion?
+    lenExt = np.random.geometric(p=1.0-extendRate, size=len(seq))-1        
+
     for i, n in enumerate(seq):
-        if n == 'N':
-            n = random.choice(nuclDict['N'])
+        if n in ['N','B','D','H','V']:
+            n = random.choice(nuclDict[n])
             majorSeq[i] = n
         
         if re.match('[ATCG]',n):
             minorSeq[i] = random.choice(nuclDict[''.join([n,str(tS[i])])])
-        
         else:
             pairs = random.sample(nuclDict[n],2)
             majorSeq[i] = pairs[0]
             minorSeq[i] = pairs[1]
+
+        if indelI[i]==1:
+            if isInsert[i] == 1:
+                minorSeq[i] = minorSeq[i] + "".join(np.random.choice(nuclDict['N'],lenExt[i]))
+            else:
+                minorSeq[i] = ''
 
     return np.array([majorSeq, minorSeq])
 
@@ -388,31 +425,55 @@ include: rnftools.include()
     
     SnakeFile.write(template.format(**context))
 
+
+def CalculateCoverage(opts, numLoci):
+
+    # dimension: num of Indiv X haplotype X num of Loci 
+    coverageMatrix = np.zeros(shape=(opts.nindiv, 2, numLoci), dtype=int)
+
+    mean = opts.meanRead
+    variance = opts.varRead
+    shape = mean*mean /variance
+    scale = variance /mean
+    nReads= np.random.gamma(shape, scale, opts.nindiv)
+
+    lociWeight = np.random.gamma(opts.ga, 1/opts.gb, numLoci)
+    lociProb = np.random.dirichlet(alpha=lociWeight, size=1)
+    for i in range(opts.nindiv):
+        coverageMatrix[i,1,] = np.random.multinomial(n=nReads[i], pvals=lociProb[0], size=1)
+        haploP = np.random.beta(opts.ha, opts.hb, numLoci)
+        for j in range(numLoci):
+            coverageMatrix[i,0,j] = np.random.binomial(n=coverageMatrix[i,1,j],p=haploP[j],size=1)
+            coverageMatrix[i,1,j] = coverageMatrix[i,1,j] - coverageMatrix[i,0,j]
+    return coverageMatrix
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='simulate SNPs on ')
+    parser = argparse.ArgumentParser(description='simulate population-based ddrad short read illumina sequence from consensus sequences')
     parser.add_argument('-i','--input',required=True, default=None,type=str,help='relative input FASTA file path')
     parser.add_argument('-ms','--ms',required=True, default=None, type=str, help='local ms path')
     parser.add_argument('-n','--nindiv',required=False, default=4,type=int,help='number of individuals')
     parser.add_argument('-l','--len',required=False, default=300, type=int, help='simulated read length')
+    #parser.add_argument('-p','--paired',required=False, default=False, type=bool, help='paired read options')
+    #parser.add_argument(-seed, '--seed', required=False, default=0, type=int, help="set random seed")
 
-    # biological/true mutation and variation 
+    # biological/true mutation and variation
+    # assumption: limit to biallelic variants for SNPs
     
     parser.add_argument('-s','--snprate',required=False, default=0.01,type=float,help='expected number of SNPs per bp')
     #parser.add_argument('-r','--recomb',required=False, default=0.5,type=double,help='recombination rate (0 to 0.5) ')
     parser.add_argument('-r','--rho',required=False, default=0,type=float,help='recombination param - 4Nr')
     parser.add_argument('-t','--tstv',required=False, default=3,type=float,help='transition to transversion ratio (>0)')
-    parser.add_argument('-smr','--smr',required=False, default=0.0001,type=float,help='expected bp rate for somatic or gamete mutation')
-    parser.add_argument('-ir', '--ir', require=False, default=0.1, type=float, help='fraction of mutations that are indels')
-    parser.add_argument('-ie', '--ie', require=False, default=0.3, type=float, help='Prob that an indel is extended')    
+    #parser.add_argument('-smr','--smr',required=False, default=0.0001,type=float,help='expected bp rate for somatic or gamete mutation') #ignoring this for now; since it is such an extreme infrequent case
+    parser.add_argument('-ir', '--ir', required=False, default=0.05, type=float, help='fraction of mutations that are indels')
+    parser.add_argument('-er', '--er', required=False, default=0.3, type=float, help='Prob that an indel is extended')    
 
-    # Experimental and Sequencing errors
-    parser.add_argument('-qp', '--qp', require=True, default=None, type=str, help='path for fq file to construct qsuality profile ')    
-    parser.add_argument('-se', '--se', require=False, default=0.1, type=float, help='fraction of sequencing errors that are indels')
+    # Experimental and sequencing errors
+    parser.add_argument('-qp', '--qp', required=False, default=None, type=str, help='path for fq file to construct quality profile ')    
+    parser.add_argument('-se', '--se', required=False, default=0.01, type=float, help='fraction of sequencing errors that are indels')
 
-    # Parameters for modulating different levels of read representations
+    # Parameters for modulating different layers in read representations
 
        # inidividual coverage
-    parser.add_argument('-l','--len',required=False, default=300, type=int, help='simulated read length')
     parser.add_argument('-mR','--meanRead',required=False, default=10000, type=int, help='expected number of total reads per individual')
     parser.add_argument('-vR','--varRead',required=False, default=1000, type=int, help='variance of number of total reads per individual')
 
@@ -422,7 +483,7 @@ if __name__ == '__main__':
 
 	# locus effect
     parser.add_argument('-ga','--ga',required=False, default=620, type=float, help='alpha parameter for gamma distrib that supports each locus-based dirichlet weight')
-    parser.add_argument('-gb','--gb',required=False, default=1/700, type=float, help='beta parameter for gamma distrib that supports each locu-based dirichlet weight ')
+    parser.add_argument('-gb','--gb',required=False, default=1.0/700, type=float, help='beta parameter for gamma distrib that supports each locu-based dirichlet weight ')
 
 
     opts = parser.parse_args()
@@ -441,12 +502,27 @@ if __name__ == '__main__':
         AllFasta.append(open("".join(["data/indiv_",str(i),".fasta"]), 'w'))
     AllFasta.append(open("data/ref/ref.fasta", 'w'))
 
-    numEntries = getNumEntries(opts.input)
+    numEntries = GetNumEntries(opts.input)
+    #print("Number of Entries ", numEntries)
     WriteVcfHeader(VcfFile, opts)
+    coverageMatrix = CalculateCoverage(opts, numEntries)
     
     dnaSeqs = readFasta(FastaFile, HeaderOut)
     for i, dnaSeq in dnaSeqs:
-        SNPit(i, numEntries, dnaSeq, AllFasta, VcfFile, opts)
+        SNPit(i, coverageMatrix, dnaSeq, AllFasta, VcfFile, opts)
+    
+    if opts.qp != None:
+        phredMatrix = GetPhred(opts.qp)
+        np.savetxt("data/ref/quality_profile.csv", phredMatrix, delimiter=',', fmt='%d')
+        np.save("~/data/ddrad/300bp_quality_profile.npy", phredMatrix)
+    else:
+        phredMatrix = np.load("~/data/ddrad/300bp_quality_profile") 
+
+    #for i in range(0,opts.nindiv):
+    #    fastqc = open("".join(["data/indiv_",str(i),".fq"]), 'w')
+    #    ConvFastaToFastQc(AllFasta[i], fastqc, phredMatrix, opts.se)
+    #    fastqc.close()
+
     
     #for i in range(opts.nindiv):
     #    makeRNFfiles(opts, i)
